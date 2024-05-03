65347aa3c76e0931f5bb7970a93b3ffbee359b78
Stefano Peretti <49915584+stefanoppp@users.noreply.github.com>
2024-05-03 12:01:45 -0300

    Add files via upload

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import pymysql
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestRegressor,GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression
 from datetime import datetime
 import numpy as np
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
@@ -33,6 +34,14 @@ def main():
         sns.pairplot(df[['Precio aceite', 'Aceitunas', 'Inflacion']])
         plt.show()
 
+    def metricas(y, y_pred):
+        mse = mean_squared_error(y, y_pred)
+        r2 = r2_score(y, y_pred)
+        mae = mean_absolute_error(y, y_pred)
+        print("Error cuad. medio: ", mse)
+        print("Coeficiente de determinación: ", r2)
+        print("Error absoluto medio: ", mae)
+
     precios = []
     aceitunas = []
     inflaciones = []
@@ -47,21 +56,34 @@ def main():
     X = np.array([aceitunas, inflaciones, fechas]).T
     y = np.array(precios)
 
-    # Entrenar el modelo
-    modelo = RandomForestRegressor(n_estimators=40, random_state=42)
+    # Entrenamiento del modelo Random forest regressor
+
+    modelo = RandomForestRegressor(n_estimators=40, random_state=5)
     modelo.fit(X, y)
 
     y_pred = modelo.predict(X)
 
-    # Obtencion de metricas
-    mse = mean_squared_error(y, y_pred)
-    r2 = r2_score(y, y_pred)
-    mae = mean_absolute_error(y, y_pred)
+    print("Metricas para modelo Random Forest Regressor")
+    metricas(y,y_pred)
 
-    print("Error cuad. medio: ", mse)
-    print("Coeficiente de determinación: ", r2)
-    print("Error absoluto medio: ", mae)
-    # Graficas
+    # Entrenamiento del modelo de Regresion lineal
+
+    print("\n")
+    modelo_lineal = LinearRegression()
+    modelo_lineal.fit(X, y)
+
+    y_pred_lineal = modelo_lineal.predict(X)
+    print("Metricas para modelo Linear Regression")
+    metricas(y,y_pred_lineal)
+
+    # Entrenamiento del modelo Gradient Boosting Regressor
+    print("\nEntrenamiento del modelo Gradient Boosting Regressor")
+    modelo_gb = GradientBoostingRegressor(n_estimators=40, learning_rate=0.1, max_depth=2, random_state=5)
+    modelo_gb.fit(X, y)
+    y_pred_gb = modelo_gb.predict(X)
+
+    print("Metricas para modelo Gradient Boosting Regressor")
+    metricas(y, y_pred_gb)
     grafica()
 
 if __name__ == "__main__":
